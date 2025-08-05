b2dc3f34a46ab32c2f1c6aeeac8c335a02f2f41c
SHORYA <chandrashorya@gmail.com>
2025-08-05 18:19:11 +0530

    Area and Circumference of a Circle

---
+++ day2.py
@@ -0,0 +1,7 @@
+# Area and Circumference of a Circle 
+r=float(input("Enter Radius of a Circle in cm:"))
+pie=3.14
+area=pie*r**2
+cir=2*pie*r
+print("Area of circle is",round(area,2),"sq cm")
+print("Circumference of circle is",round(cir,2),"cm")
