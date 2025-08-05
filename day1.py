64cd1269e39aeb59c38048e9f3c8bd4a07dfc46a
SHORYA <chandrashorya@gmail.com>
2025-08-05 18:18:23 +0530

    Program To Print Your Introduction

---
+++ day1.py
@@ -0,0 +1,7 @@
+#Program To Print Your Introduction
+print("------------------------------------")
+print("Hello  my name is Shorya")
+print("I am in BCA 2nd Year")
+print("I am Studing hardly")
+print("I want to become Software engineer")
+print("------------------------------------")
